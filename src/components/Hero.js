c125eb4d78d22c3a79b34a6dbb201a4e9679ee72
cammac <camm@rocktechcomputer.com>
2020-06-01 14:19:20 -0700

    first commit, scaffold and setup

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='hero'>
+      <button className='hero-btn'>Admin portal</button>
+      <button className='hero-btn'>User portal</button>
+    </div>
+  )
+}
+
+export default Hero;
