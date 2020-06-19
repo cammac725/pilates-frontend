f33075d28dd12b321b3e74247101075195c9e8ff
cammac <camm@rocktechcomputer.com>
2020-06-19 12:50:16 -0700

    dashboard content and styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 export default function Dashboard() {
   return (
-    <div>
-      <h1>My Dashboard</h1>
+    <div className='admin-dashboard'>
+      
+      <div className='panel'>
+        <h1 className='title'>Admin Dashboard</h1>
+        <div className='content'>
+          <div className='clients'>
+            <h2>Clients</h2>
+            <button className='hero-btn'>See all</button>
+          </div>
+          <div className='exers'>
+            <h2>Exercises</h2>
+            <button className='hero-btn'>See all</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
