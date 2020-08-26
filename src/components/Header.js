d82c78f607ead309afb507320bf990c75176b3a2
cammac <camm@rocktechcomputer.com>
2020-08-26 14:03:27 -0700

    added dashboard button to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/pbms.webp';
 
 export default function Header() {
@@ -6,6 +7,7 @@ export default function Header() {
     <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <NavLink to='/dashboard' className='header-link'>Dashboard</NavLink>
         <a href="https://pilatesbodyandmindstudio.com/">
           Pilates Body & Mind Home
         </a>
